864f80367e1436fc8fdd305a4fdd8915b424bb8e
Cong <CongLe11@gmail.com>
2024-08-18 15:33:01 +0100

    adds homework for lesson 2

---
+++ lesson 2/homework/app.js
@@ -0,0 +1,59 @@
+// Exercise 4: Even Numbers Pattern
+function runExercise4() {
+    let output = '';
+
+    /// Example answer
+    for (let i = 2; i <= 10; i += 2) {
+        output += 'Hello ' + i + '<br>';
+    }
+
+    document.getElementById('output4').innerHTML = output;
+}
+
+// Exercise 5: Squares of Numbers
+function runExercise5() {
+    let output = 'something';
+
+
+    document.getElementById('output5').innerHTML = output;
+}
+
+// Exercise 6: Increasing Dots Pattern
+function runExercise6() {
+    let output = 'something';
+
+
+    document.getElementById('output6').innerHTML = output;
+}
+
+// Exercise 7: Increasing Numbers Pattern
+function runExercise7() {
+    let output = 'something';
+
+
+    document.getElementById('output7').innerHTML = output;
+}
+
+// Exercise 8: Incremental Sequence Pattern
+function runExercise8() {
+    let output = 'something';
+
+
+    document.getElementById('output8').innerHTML = output;
+}
+
+// Exercise 9: Decreasing Sequence Pattern
+function runExercise9() {
+    let output = 'something';
+
+
+    document.getElementById('output9').innerHTML = output;
+}
+
+// Exercise 10: Factorials
+function runExercise10() {
+    let output = 'something';
+
+
+    document.getElementById('output10').innerHTML = output;
+}
